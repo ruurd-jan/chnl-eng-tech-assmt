54fc12b6f0aa5392fc4d7e7ab098a79954866a73
Luz Parra <luz.x.parra.-nd@disney.com>
2022-02-08 05:16:40 +0000

    Call methods  from the web application

--- Chnl.Eng.Tech.Assmt/Chnl.Eng.Tech.Assmt.WebApp/Controllers/HomeController.cs
+++ Chnl.Eng.Tech.Assmt/Chnl.Eng.Tech.Assmt.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Chnl.Eng.Tech.Assmt.WebApp.Models;
+﻿using Chnl.Eng.Tech.Assmt.BL.BL;
+using Chnl.Eng.Tech.Assmt.BL.Model;
+using Chnl.Eng.Tech.Assmt.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,6 +14,8 @@ namespace Chnl.Eng.Tech.Assmt.WebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _apikey = "541b989ef78ccb1bad630ea5b85c6ebff9ca3322";
+        private readonly string _url = "https://api-dev.channelengine.net/api/v2/";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,7 +24,26 @@ namespace Chnl.Eng.Tech.Assmt.WebApp.Controllers
 
         public IActionResult Index()
         {
-            return View();
+
+
+            return View(GetProductsFromOrdersByStatusAsync());
+        }
+
+        public IActionResult SetStockOfProduct(string id)
+        {
+            List<Offer> offers = new List<Offer>()
+                { new Offer() { MerchantProductNo =id, Stock = 25 } };
+            ProductBL productBO = new ProductBL(_url, _apikey);
+            ViewData["Message"] = productBO.SetStockOfProduct(offers);
+            return View("Index", GetProductsFromOrdersByStatusAsync());
+
+        }
+
+        public List<Product> GetProductsFromOrdersByStatusAsync()
+        {
+            ProductBL productBO = new ProductBL(_url, _apikey);
+            List<Product> lstProduct = productBO.GetTopFive("IN_PROGRESS");
+            return lstProduct;
         }
 
         public IActionResult Privacy()
